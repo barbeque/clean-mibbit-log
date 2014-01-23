a408f33dcad69a89f690ac1e188eb575894d65a2
mike stedman <github@ravuya.com>
2014-01-22 21:03:00 -0700

    Fixed up the comments to show my lack of understanding of the api.

--- Cleaner.py
+++ Cleaner.py
@@ -5,13 +5,15 @@ class CleanMibbitLogCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		# grab everything from the current buffer...
 		reg = sublime.Region(0, self.view.size())
-		# split it up by lines
+		# split it up by lines... but i'm not really sure if it actually does this
 		lines = self.view.split_by_newlines(reg)
 		for lineRegion in lines:
-			# do our work to it
+			# why do i have to do this twice?
+			# maybe because the buffer changed since the last time i called this region...
+			# ... so the absolute character position is wrong again.
+			# this SHOULD work properly by getting the line that contains the region.
 			realLineRegion = self.view.line(lineRegion)
 			text = self.view.substr(realLineRegion)
-			print text
 			timestampsStripped = self.removeTimestamps(text)
 			usernamesReformatted = self.formatUsernames(timestampsStripped)
 			# replace the buffer with the results.
