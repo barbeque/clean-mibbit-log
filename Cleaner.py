5b8c973b18f0ab81294dd7dbb8ebd62ea1f6c5cb
mike stedman <github@ravuya.com>
2014-01-22 21:00:47 -0700

    Maybe fixed the timestamp-inside-chat bug

--- Cleaner.py
+++ Cleaner.py
@@ -5,15 +5,20 @@ class CleanMibbitLogCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		# grab everything from the current buffer...
 		reg = sublime.Region(0, self.view.size())
-		text = self.view.substr(reg)
-		# do our work to it
-		timestampsStripped = self.removeTimestamps(text)
-		usernamesReformatted = self.formatUsernames(timestampsStripped)
-		# replace the buffer with the results.
-		self.view.replace(edit, reg, usernamesReformatted)
+		# split it up by lines
+		lines = self.view.split_by_newlines(reg)
+		for lineRegion in lines:
+			# do our work to it
+			realLineRegion = self.view.line(lineRegion)
+			text = self.view.substr(realLineRegion)
+			print text
+			timestampsStripped = self.removeTimestamps(text)
+			usernamesReformatted = self.formatUsernames(timestampsStripped)
+			# replace the buffer with the results.
+			self.view.replace(edit, realLineRegion, usernamesReformatted)
 	def removeTimestamps(self, logText):
 		# replace [0-9]+:[0-9]+\t with nothing
-		return re.sub('[0-9]+:[0-9]+\t', '', logText)
+		return re.sub('^[0-9]+:[0-9]+\t', '', logText)
 	def formatUsernames(self, logText):
 		# replace [A-z]+\t, putting brackets about.
 		# raw strings (the r'') are very important here.
